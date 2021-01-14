fdb57ebf82cd408af679204e9bb89400f8aed398
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-14 20:53:45 -0300

    Enviando dados digitados entre duas activities

--- app/src/main/java/br/com/fabricadesinapse/fabricasinapse_live_android_introducao/MainActivity.kt
+++ app/src/main/java/br/com/fabricadesinapse/fabricasinapse_live_android_introducao/MainActivity.kt
@@ -1,10 +1,11 @@
 package br.com.fabricadesinapse.fabricasinapse_live_android_introducao
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +15,25 @@ class MainActivity : AppCompatActivity() {
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
         val etNome = findViewById<EditText>(R.id.etNome)
         val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val btEnviar2 = findViewById<Button>(R.id.btEnviar2)
 
         btEnviar.setOnClickListener {
             if (etNome.text.isNotBlank()) {
                 tvResultado.text = getString(R.string.hello_name, etNome.text.toString())
+            } else {
+                etNome.error = getString(R.string.type_your_name)
+            }
+        }
+
+        btEnviar2.setOnClickListener {
+            if (etNome.text.isNotBlank()) {
+                val nomeDigitado = etNome.text.toString()
+
+                val intent = Intent(this, ResultActivity::class.java)
+
+                intent.putExtra("NOME_DIGITADO", nomeDigitado)
+
+                startActivity(intent)
             } else {
                 etNome.error = getString(R.string.type_your_name)
             }
--- app/src/main/java/br/com/fabricadesinapse/fabricasinapse_live_android_introducao/ResultActivity.kt
+++ app/src/main/java/br/com/fabricadesinapse/fabricasinapse_live_android_introducao/ResultActivity.kt
@@ -1,11 +1,18 @@
 package br.com.fabricadesinapse.fabricasinapse_live_android_introducao
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val nomeDigitado = intent.getStringExtra("NOME_DIGITADO")
+
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        tvResultado.text = nomeDigitado
     }
 }
