429c41dd8133c5b9f94d21fb948ca4d8aa6908ea
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-14 20:44:53 -0300

    ResultActivity criada

---
+++ app/src/main/java/br/com/fabricadesinapse/fabricasinapse_live_android_introducao/ResultActivity.kt
@@ -0,0 +1,11 @@
+package br.com.fabricadesinapse.fabricasinapse_live_android_introducao
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
