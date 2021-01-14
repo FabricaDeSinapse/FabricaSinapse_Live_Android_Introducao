1d3bca21294f580cb388ebcd11068cbe9414d7c7
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-14 20:38:15 -0300

    Navigation Drawer Activity criada

---
+++ app/src/main/java/br/com/fabricadesinapse/fabricasinapse_live_android_introducao/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.fabricadesinapse.fabricasinapse_live_android_introducao.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/br/com/fabricadesinapse/fabricasinapse_live_android_introducao/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package br.com.fabricadesinapse.fabricasinapse_live_android_introducao.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
